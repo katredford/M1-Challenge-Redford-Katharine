254fbf8fed2c11d2a4ffb29b6294901c5013a59c
katredford <katharine.redford@gmail.com>
2022-02-18 16:28:46 -0600

    make calculator dynamic

--- CalculatorObject/src/main/java/com/company/Calculator.java
+++ CalculatorObject/src/main/java/com/company/Calculator.java
@@ -3,36 +3,54 @@ package com.company;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        int result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+        return result;
     }
 
     public double add(double a, double b) {
-        return a + b;
+        double result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+        return result;
+
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        int result = a - b;
+        System.out.println(a + " - " + b + " = " + result);
+        return result;
+
     }
 
     public float subtract(float a, float b) {
-        return a - b;
+        float result = a - b;
+        System.out.println(a + " - " + b + " = " + result);
+        return result;
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        int result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+        return result;
     }
 
     public double multiply(double a, double b){
-        return a * b;
+        double result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+        return result;
     }
 
 
     public int divide(int a, int b) {
-        return a / b;
+        int result = a / b;
+        System.out.println(a + " / " + b + " = " + result);
+        return result;
     }
 
     public double divide(double a, double b) {
-        return a / b;
+        double result = a / b;
+        System.out.println(a + " / " + b + " = " + result);
+        return result;
     }
 
 }
